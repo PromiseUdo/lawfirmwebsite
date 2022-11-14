836944b4f87aa9b2443fe5301c24cd58af6d15d8
PromiseUdo <info.promiseudo@gmail.com>
2022-11-14 08:34:59 +0100

    first commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,42 +1,42 @@
-import React from 'react';
+import React from "react";
 
 // import social
-import { social } from '../data';
+import { social } from "../data";
 
 const Contact = () => {
   return (
     <section
-      id='contact'
-      className='bg-primary text-white min-h-[732px] section'
+      id="contact"
+      className="bg-primary text-white min-h-[732px] section"
     >
-      <div className='container mx-auto text-center'>
-        <h2 className='text-5xl font-primary font-extrabold mb-4'>
+      <div className="container mx-auto text-center">
+        <h2 className="text-5xl font-primary font-extrabold mb-4">
           Contact us
         </h2>
-        <p className='max-w-[540px] mx-auto px-6 lg:px-0 mb-[64px]'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh
-          tristique ipsum.
+        <p className="max-w-[540px] mx-auto px-6 lg:px-0 mb-[64px]">
+          For all other enquiries, please complete the form and we will email a
+          reply to you as soon as possible
         </p>
 
         {/* form */}
-        <form className='px-8 lg:px-0 max-w-[600px] mx-auto flex flex-col space-y-6 mb-[46px]'>
-          <input className='form-control' type='text' placeholder='Full name' />
+        <form className="px-8 lg:px-0 max-w-[600px] mx-auto flex flex-col space-y-6 mb-[46px]">
+          <input className="form-control" type="text" placeholder="Full name" />
           <input
-            className='form-control'
-            type='text'
-            placeholder='Email address'
+            className="form-control"
+            type="text"
+            placeholder="Email address"
           />
-          <textarea className='textarea' placeholder='Message'></textarea>
-          <button className='btn bg-accent hover:bg-accent-hover transition-all'>
+          <textarea className="textarea" placeholder="Message"></textarea>
+          <button className="btn bg-accent hover:bg-accent-hover transition-all">
             Send Message
           </button>
         </form>
 
         {/* social */}
-        <div className='flex items-center justify-between mx-auto max-w-[205px]'>
+        <div className="flex items-center justify-between mx-auto max-w-[205px]">
           {social.map((item, index) => {
             return (
-              <a href='#' key={index}>
+              <a href="#" key={index}>
                 <img src={item.icon}></img>
               </a>
             );
